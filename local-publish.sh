25d7a5ab0593ea86ae33f91230a3105722a0f3c9
rzymek <krzysztof.rzymkowski@gmail.com>
2026-01-08 13:32:02 +0100

    Refactor: Replace `dev-publish.sh` with `local-publish.sh` for local Verdaccio setup and streamlined publishing workflow.

---
+++ local-publish.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+docker stop npmrepo
+set -eux
+docker run --network host --detach --rm --name npmrepo verdaccio/verdaccio
+until nc -z localhost 4873; do sleep 1; done
+export NPM_PASS="$(openssl rand -base64 12)";
+export NPM_USER=local;
+export NPM_EMAIL=local@localhost.local;
+pnpx npm-cli-login -r http://localhost:4873/
+AUTH=$(echo -n "$NPM_USER:$NPM_PASS" | base64 -w 0)
+npm config set //localhost:4873/:_auth $AUTH --registry http://localhost:4873/
+
+npm publish --registry http://localhost:4873/ --provenance=false
+
